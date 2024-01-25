3ae5d561f4ae57c9bc51fe869008e752345b6ba2
DYAlex <yvette.demidova@yandex.ru>
2024-01-26 02:28:20 +0300

    Changed App to call beans and compare them by reference

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,23 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/*
+Задание:
+1. + Прочитайте про виды бинов.
+
+2. + Создайте POJO-класс Cat.
+
+3. + В классе AppConfig, по аналогии, создайте бин с именем “cat”.
+4. + Настройте этот бин так, чтобы он создавался новым при каждом запросе.
+
+5. + В классе App, по аналогии, вызовите еще раз бин HelloWorld,
++ затем 2 раза вызовите бин cat.
++ Сравните 2 пары бинов по ссылке и выведите результат в консоль.
++ Для пары HelloWorld должно вернуться true, для cat - false.
+Так вы можете увидеть результат того, как работает наш контейнер.
+
+6. Раскомментируйте тестовый класс и проверьте своё решение.
+ */
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +25,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanSecond = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCatFirst = (Cat) applicationContext.getBean("cat");
+        Cat beanCatSecond = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(beanSecond.getMessage());
+        System.out.println(beanCatFirst.getName());
+        System.out.println(beanCatSecond.getName());
+
+        System.err.println(bean == beanSecond);
+        System.err.println(beanCatFirst == beanCatSecond);
     }
 }
